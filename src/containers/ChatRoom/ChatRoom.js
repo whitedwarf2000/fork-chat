92a901be9cad1eda67b4168c28fbcb5b7bf3d5a5
Son Le <sontdt69@gmail.com>
2021-06-09 16:11:19 +0700

    Implement chat room using socketIOClient

--- src/containers/ChatRoom/ChatRoom.js
+++ src/containers/ChatRoom/ChatRoom.js
@@ -1,11 +1,50 @@
-import React from 'react';
-
+import React, { useState, useRef } from 'react';
 import { useParams } from 'react-router-dom';
+
+import useChat from './useChat';
 
 const ChatRoom = () => {
   const { id } = useParams();
+  const textAreaRef = useRef(null);
 
-  return <h2>Welcome to chat room {id}</h2>;
+  const { messages, sendMessage } = useChat(id);
+  const [currentMess, setCurrentMess] = useState('');
+
+  const handleSendMessage = () => {
+    if (!textAreaRef.current) {
+      return;
+    }
+    sendMessage(textAreaRef.current.value);
+    setCurrentMess('');
+  };
+
+  return (
+    <div>
+      <h2>Welcome to chat room {id}</h2>
+      <div>
+        <ol>
+          {messages.map((message, i) => (
+            <li
+              key={i}
+              className={`message-item ${
+                message.ownedByCurrentUser ? 'my-message' : 'received-message'
+              }`}>
+              {message.body}
+            </li>
+          ))}
+        </ol>
+      </div>
+      <textarea
+        ref={textAreaRef}
+        defaultValue={currentMess}
+        placeholder="Write message..."
+        className="new-message-input-field"
+      />
+      <button onClick={handleSendMessage} className="send-message-button">
+        Send
+      </button>
+    </div>
+  );
 };
 
 export default ChatRoom;
