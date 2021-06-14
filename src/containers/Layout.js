f4f7c181ba56673e27692148c13b1324366dbd4c
Son Le <sontdt69@gmail.com>
2021-06-14 13:15:55 +0700

    Create private route; Mock authorization hook; Add eslintignore

--- src/containers/Layout.js
+++ src/containers/Layout.js
@@ -1,18 +1,22 @@
 import React from 'react';
-import { Switch, Route } from 'react-router-dom';
+import { Switch } from 'react-router-dom';
 
 import dynamic from 'utils/dynamic';
 
+import RouteWrapper from './RouterWrapper';
+
+const Login = dynamic(() => import('containers/Login'));
 const Home = dynamic(() => import('containers/Home'));
 const NotFound = dynamic(() => import('containers/NotFound'));
 
 const Layout = () => {
   return (
     <Switch>
-      <Route exact path="/" component={Home} />
-      <Route path="*">
+      <RouteWrapper exact isPrivate path="/" component={Home} />
+      <RouteWrapper path="/login" component={Login} />
+      <RouteWrapper path="*">
         <NotFound />
-      </Route>
+      </RouteWrapper>
     </Switch>
   );
 };
