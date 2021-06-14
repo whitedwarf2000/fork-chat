a8ec2ffe5c861461ee73857d6291b4c2ead7edd4
Son Le <sontdt69@gmail.com>
2021-06-14 10:32:00 +0700

    Add fork-ui to project

--- src/index.js
+++ src/index.js
@@ -2,5 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 import App from './App';
+import 'fork-ui/styles/index.scss';
+import './index.scss';
 
 ReactDOM.render(<App />, document.getElementById('root'));
