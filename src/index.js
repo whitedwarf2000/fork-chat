6f8dee02cd4559f51853672e82943af719dc34c1
Son Le <sontdt69@gmail.com>
2021-06-16 09:23:46 +0700

    Config alias fork-ui

--- src/index.js
+++ src/index.js
@@ -2,7 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 import App from './App';
-import 'fork-ui/styles/index.scss';
+
+import '@fork-ui/styles/index.scss';
 import './scss/index.scss';
 
 ReactDOM.render(<App />, document.getElementById('root'));
